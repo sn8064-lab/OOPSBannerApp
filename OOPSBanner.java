0d5dc5f86748c469052500fa7c119a33436209b0
S.NITHISHA <sn8064@srmist.edu.in>
2026-02-19 00:22:02 +0530

    UC2: Printed OOPS banner using ASCII pattern

--- OOPSBanner.java
+++ OOPSBanner.java
@@ -2,7 +2,14 @@ public class OOPSBanner {
 
     public static void main(String[] args) {
 
-        System.out.println("OOPS");
+        System.out.println(" *****   *****   *****   ***** ");
+        System.out.println("*     * *     * *     * *     *");
+        System.out.println("*     * *     * *     * *      ");
+        System.out.println("*     * *****   *****   *****  ");
+        System.out.println("*     * *       *             *");
+        System.out.println("*     * *       *       *     *");
+        System.out.println(" *****  *       *****    ***** ");
 
     }
+
 }
